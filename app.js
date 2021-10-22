07aeb47056d982ab5a63e75e44c6ae2d58c05da5
Victor Dubbeld <vicdubbeld@gmail.com>
2021-10-21 22:35:52 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      firstName: "John",
+      lastName: "Doe",
+      email: "john@email.com",
+      gender: "male",
+      picture: "https://randomuser.me/api/portraits/men/8.jpg",
+    };
+  },
+  methods: {
+    async getUser() {
+      const res = await fetch("https://randomuser.me/api");
+      const { results } = await res.json();
+
+      console.log(results);
+
+      (this.firstName = results[0].name.first),
+        (this.lastName = results[0].name.last),
+        (this.email = results[0].email),
+        (this.gender = results[0].gender),
+        (this.picture = results[0].picture.large);
+    },
+  },
+});
+
+// <variable_name>.mount("_selector_")
+app.mount("#app");
